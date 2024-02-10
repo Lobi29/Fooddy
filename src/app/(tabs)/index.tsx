565b570c5fe7cabf3c2678ffd2a000c9a74e1817
Chao Lobi <gogoiladitya@gmail.com>
2024-02-10 16:59:11 +0530

    type safe components added

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,78 +1,12 @@
-// import { StyleSheet } from 'react-native';
+import { View } from "react-native";
+import products from "@assets/data/products";
+import ProductListItem from "@components/ProductListItem";
 
-// import EditScreenInfo from '@/src/components/EditScreenInfo';
-// import { Text, View } from '@/src/components/Themed';
-
-// export default function TabOneScreen() {
-//   return (
-//     <View style={styles.container}>
-//       <Text style={styles.title}>Hello Laditya</Text>
-//       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-//       <EditScreenInfo path="app/(tabs)/index.tsx" />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   title: {
-//     fontSize: 20,
-//     fontWeight: 'bold',
-//   },
-//   separator: {
-//     marginVertical: 30,
-//     height: 1,
-//     width: '80%',
-//   },
-// });
-
-import { StyleSheet, Image, Text, View } from "react-native";
-
-
-import products from "@/assets/data/products";
-import Colors from "@/src/constants/Colors";
-const product = products[0];
-
-
-export default function TabOneScreen() {
+export default function MenuScreen() {
   return (
-    <View style={styles.container}>
-      <Image
-        source={{ uri: product.image }}
-        style={styles.image}
-        resizeMode="contain"
-      />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price.toFixed(2)}</Text>
-
+    <View>
+      <ProductListItem product={products[3]} />
+      <ProductListItem product={products[1]} />
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 10,
-    overflow: 'hidden',
-  },
-  image: {
-    width: '100%',
-    aspectRatio: 1,
-    alignSelf: 'center',
-  },
-  title: {
-    fontWeight: '600',
-    fontSize: 18,
-    marginVertical: 10,
-  },
-  price: {
-    color: Colors.light.tint,
-    fontWeight: 'bold',
-    marginTop: 'auto',
-  },
-});
